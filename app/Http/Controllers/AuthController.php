8e1ac9a29f5e3777d38f77559020b86f52a18364
Chio <raharjocipto4@gmail.com>
2023-06-24 09:21:37 +0800

    mengatur migrasi login dan logout

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+{
+    // Validasi input
+    $request->validate([
+        'id_card_number' => 'required',
+        'password' => 'required',
+    ]);
+
+    // Lakukan logika autentikasi di sini
+    // Misalnya, periksa ID Card Number dan Password di database
+
+    // Jika autentikasi berhasil
+    if ($authSuccess) {
+        $user = // Ambil data user dari database
+
+        // Buat token
+        $token = // Generate token menggunakan library JWT atau Laravel Passport
+
+        // Buat respon dengan format yang diinginkan
+        $response = [
+            'username' => $user->name,
+            'password' => $password,
+            'token' => $token
+        ];
+
+        // Mengembalikan respon sukses dengan kode status 200
+        return response()->json($response);
+    }
+
+    // Jika autentikasi gagal
+    // Mengembalikan respon error dengan kode status 401
+    return response()->json(['message' => 'ID Card Number or Password incorrect'], 401);
+}
+
+}
--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,30 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+Route::post('/auth/login', 'AuthController@login');
 Route::middleware('/api/v1/auth/login')->get('/user', function(Request $request){
     return $request->users();
+$idCardNumber = 'SomeText';
+$password = 'SomeText';
+
+$response = Http::post('https://[domain]/api/v1/auth/login', [
+    'id_card_number' => $idCardNumber,
+    'password' => $password,
+]);
+
+if ($response->ok()) {
+    // Login berhasil
+    $responseData = $response->json();
+    $accessToken = $responseData['access_token'];
+    
+    // Lakukan tindakan selanjutnya setelah login sukses, misalnya mengirim permintaan untuk melamar pekerjaan
+    // ...
+} else {
+    // Login gagal
+    $errorResponse = $response->json();
+    $errorMessage = $errorResponse['message'];
+    // Lakukan tindakan yang sesuai untuk penanganan kesalahan login
+    // ...
+}
+
 });
